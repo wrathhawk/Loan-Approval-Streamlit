bada4ccdd5c8829a10a506c464c83bbff4c99f79
wrathhawk <103183778+wrathhawk@users.noreply.github.com>
2025-04-09 12:09:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ with open('encoder.pkl', 'rb') as f:
 st.title("Loan Approval Prediction App")
 
 # User input
-home_ownership = st.selectbox("Home Ownership", ["MORTGAGE", "RENT", "OWN", "OTHER"])
+person_home_ownership = st.selectbox("Home Ownership", ["MORTGAGE", "RENT", "OWN", "OTHER"])
 loan_intent = st.selectbox("Loan Intent", ["PERSONAL", "EDUCATION", "MEDICAL", "VENTURE", "HOMEIMPROVEMENT", "DEBTCONSOLIDATION"])
 loan_grade = st.selectbox("Loan Grade", ["A", "B", "C", "D", "E", "F", "G"])
-default_on_file = st.selectbox("Default on File", ["Y", "N"])
+cb_person_default_on_file = st.selectbox("Default on File", ["Y", "N"])
 
 income = st.number_input("Annual Income", min_value=0)
 loan_amount = st.number_input("Loan Amount", min_value=0)
@@ -30,7 +30,7 @@ loan_interest = st.number_input("Loan Interest Rate (in %)", min_value=0.0)
 age = st.number_input("Age of the Applicant", min_value=18)
 
 # Prepare input for encoding
-cols_to_encode = [[home_ownership, loan_intent, loan_grade, default_on_file]]
+cols_to_encode = [[person_home_ownership, loan_intent, loan_grade, cb_person_default_on_file]]
 
 # Encode categorical
 encoded = encoder.transform(cols_to_encode)
@@ -39,12 +39,13 @@ encoded = encoder.transform(cols_to_encode)
 categorical_columns = encoder.get_feature_names_out()  # exact order from training
 df_cat_encoded = pd.DataFrame(encoded, columns=categorical_columns)
 
-numeric = pd.DataFrame([[age, income, person_emp_lenght, loan_amount, loan_interest, loan_percent_income, cred_hist_lenght]],
-                       columns=["age", "income", "person_emp_lenght", "loan_amount", "loan_interest", "loan_percent_income", "cred_hist_lenght"])
+numeric = [[age, income, person_emp_length, loan_amount, loan_interest, loan_percent_income, cred_hist_length]]
 
 # Prepare numeric data for scaling
-numeric_df = pd.DataFrame(numeric, columns=['person_age', 'person_income', 'person_emp_length', 'loan_amnt', 
-                                            'loan_int_rate', 'loan_percent_income', 'cb_person_cred_hist_length'])
+numeric_df = pd.DataFrame(numeric, columns=[
+    'person_age', 'person_income', 'person_emp_length', 'loan_amnt',
+    'loan_int_rate', 'loan_percent_income', 'cb_person_cred_hist_length'
+])
 
 final_input_data = pd.concat([numeric_df, df_cat_encoded], axis=1)
 
